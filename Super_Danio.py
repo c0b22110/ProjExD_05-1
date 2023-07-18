98c4a3f98637994e3396e0db93157d887d1d500f
Masaki <c0a22080fa@edu.teu.ac.jp>
2023-07-18 14:05:31 +0900

    うんこの出現4

--- Super_Danio.py
+++ Super_Danio.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import pygame as pg
+from pygame.math import Vector2
 
 
 def main():
@@ -13,6 +14,8 @@ def main():
     kk_img = pg.image.load("ex05/fig/danieru.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 0.5)
     bg_imgs = [bg_img, pg.transform.flip(bg_img, True, False)] * 2
+    
+    game_over_font = pg.font.Font(None, 64)  # ゲームオーバーメッセージ用のフォント
 
     player_y = 400
     player_y_vel = 0
@@ -23,10 +26,12 @@ def main():
     class Obstacle:
         def __init__(self):
             self.x = 800
-            self.y = 400
+            self.y = 480
             self.speed = 5
             self.active = True
             self.image = pg.image.load("ex05/fig/unnko.png")
+            self.image = pg.transform.rotozoom(self.image, 0, 0.3)
+            self.rect = self.image.get_rect()
 
         def update(self):
             self.x -= self.speed
@@ -38,26 +43,44 @@ def main():
             self.active = True
 
         def check_collision(self):
-            global player_y  # player_y をグローバル変数として参照
-            if self.active and abs(player_y - self.y) < 50:
-                player_y = 400
-                player_y_vel = 0
-                self.active = False
+            global player_y, running# player_y をグローバル変数として参照
+            if self.active:
+                # オブジェクトの中心座標と半径を計算
+                obstacle_center = Vector2(self.rect.center)
+                obstacle_radius = self.rect.width / 2
+
+                kk_center = Vector2(200 + kk_img.get_width() / 2, player_y + kk_img.get_height() / 2)
+                kk_radius = kk_img.get_width() / 2
+
+                # 中心座標間の距離を計算
+                distance = kk_center.distance_to(obstacle_center)
+
+                # 距離が半径の和より小さい場合は衝突と判定
+                if distance < obstacle_radius + kk_radius:
+                    running = False
 
         def draw(self):
             screen.blit(self.image, [self.x, self.y])
 
     obstacle = Obstacle()
+    running = True
 
-    while True:
+    def draw_game_over():
+        game_over_text = game_over_font.render("Game Over", True, (255, 0, 0))
+        screen.blit(game_over_text, (300, 250))
+
+    while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                return
+                running = False
+
+            
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE and not jumping:
                     player_y_vel = -15
                     jumping = True
 
+                
         x = tmr % 3200
         screen.blit(bg_img, [x, 0])
 
@@ -74,13 +97,20 @@ def main():
 
         obstacle.update()
         obstacle.check_collision()
-        obstacle.draw()
 
-        screen.blit(kk_img, [200, player_y])
+        if running:
+            obstacle.draw()
+            screen.blit(kk_img, [200, player_y])
+        else:
+            draw_game_over()
+
         pg.display.update()
         tmr += 1
         clock.tick(100)
 
+    # ゲームループ終了後にゲームオーバー画面を表示するなどの処理を追加することができます
+    # 以下は単純にウィンドウを閉じるだけの処理です
+    
 
 if __name__ == "__main__":
     pg.init()
